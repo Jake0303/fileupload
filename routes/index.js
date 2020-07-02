b7a9544b0dd19230e3f50bfb2f0b288791eb482e
Jacob Amaral <jake0303@hotmail.ca>
2020-07-02 15:48:47 -0400

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var formidable = require('formidable');
+var util = require('util');
+var fs = require('fs');
+var path = require('path');
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+/*POST upload page. */
+router.post('/upload', function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.uploadDir = path.join(__dirname, '../public/images');
+    form.parse(req, function (err, fields, files) {
+        //Update filename
+        files.upload.name = fields.title + '.' + files.upload.name.split('.')[1];
+        console.log(files.upload.name);
+        //Upload file on our server
+        fs.rename(files.upload.path, path.join(form.uploadDir, files.upload.name), function (err) {
+            if (err) console.log(err);
+        });
+        console.log('Received upload');
+    });
+    form.on('end', function (err, fields, files) {
+        console.log('File successfuly uploaded');
+        res.end('File successfuly uploaded');
+    });
+});
+
+module.exports = router;
